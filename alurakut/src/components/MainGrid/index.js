e8dd59921ca46a1c4d4b86dc115568bfedad3a1a
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2021-07-14 18:20:46 -0300

    segunda aula

--- alurakut/src/components/MainGrid/index.js
+++ alurakut/src/components/MainGrid/index.js
@@ -17,7 +17,7 @@ const MainGrid = styled.main`
     display: grid;
     grid-template-areas: 
     "profileArea welcomeArea profileRelationsArea";
-    grid-template-columns: 160px 1fr 312px;
+    grid-template-columns: 180px 1fr 312px;
   }
 `;
 
